3e4b208039745f251fbcb3c41b19dc2e68aaab58
martin-mcinerney <martin_mcinerney@live.ie>
2023-01-09 11:57:14 +0000

    Set static root

--- the_junior_tech_society/settings.py
+++ the_junior_tech_society/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,6 +138,8 @@ USE_TZ = True
 STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
