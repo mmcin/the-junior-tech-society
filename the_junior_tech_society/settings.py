4530c084d7952ef17a16a29f8b68f92e2a1af053
martin-mcinerney <martin_mcinerney@live.ie>
2023-01-09 12:47:00 +0000

    adds secret key env variable

--- the_junior_tech_society/settings.py
+++ the_junior_tech_society/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%18qun607b4ms#djqq#4pdqay@^5y2_lcgqx%!+@eq+)d^+3n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
